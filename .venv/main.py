f6d8f13f22d67525c3f07b0023c07eea38611d83
rashonwhite <rashonwhite684@gmail.com>
2025-09-26 13:39:55 -0500

    feat: add jsonable_encoder funcaitonlity

--- .venv/main.py
+++ .venv/main.py
@@ -1,25 +1,22 @@
-from fastapi import FastAPI, Request
+from datetime import datetime
+
+from fastapi import FastAPI
 from fastapi.encoders import jsonable_encoder
-from fastapi.exceptions import RequestValidationError
-from fastapi.responses import JSONResponse
 from pydantic import BaseModel
+
+fake_db = {}
+
+
+class Item(BaseModel):
+    title: str
+    timestamp: datetime
+    description: str | None = None
+
 
 app = FastAPI()
 
-@app.exception_handler(RequestValidationError)
-async def validation_exception_handler(request: Request, exc: RequestValidationError):
-    return JSONResponse(
-        status_code=422,
-        content=jsonable_encoder({
-            "detail": exc.errors(),
-            "body": exc.body
-        })
-    )
-    
-class Item(BaseModel):
-    title: str
-    size: int
 
-@app.post("/items/")
-async def create_item(item: Item):
-    return Item
+@app.put("/items/{id}")
+def update_item(id: str, item: Item):
+    json_compatible_item_data = jsonable_encoder(item)
+    fake_db[id] = json_compatible_item_data
