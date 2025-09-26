fdd2428fb0d27233b070b9a6028c5395675b63e4
rashonwhite <rashonwhite684@gmail.com>
2025-09-26 12:06:38 -0500

    feat: add path operation decorator with status_code

--- .venv/main.py
+++ .venv/main.py
@@ -1,25 +1,15 @@
-from fastapi import FastAPI, Request
-from fastapi.encoders import jsonable_encoder
-from fastapi.exceptions import RequestValidationError
-from fastapi.responses import JSONResponse
+from fastapi import FastAPI, status
 from pydantic import BaseModel
 
 app = FastAPI()
 
-@app.exception_handler(RequestValidationError)
-async def validation_exception_handler(request: Request, exc: RequestValidationError):
-    return JSONResponse(
-        status_code=422,
-        content=jsonable_encoder({
-            "detail": exc.errors(),
-            "body": exc.body
-        })
-    )
-    
 class Item(BaseModel):
-    title: str
-    size: int
-
-@app.post("/items/")
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+    tags: set[str] = set()
+    
+@app.post("/items/", response_model=Item, status_code=status.HTTP_201_CREATED)
 async def create_item(item: Item):
-    return Item
+    return item
