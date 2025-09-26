fadb2e713a09ab71922535106dbbac4c7c3c0c0e
rashonwhite <rashonwhite684@gmail.com>
2025-09-26 12:14:48 -0500

    feat: add path operation decorator with deprecated attribute

--- .venv/main.py
+++ .venv/main.py
@@ -1,23 +1,18 @@
-from fastapi import FastAPI, status
-from pydantic import BaseModel
+from fastapi import FastAPI
 
 app = FastAPI()
 
-class Item(BaseModel):
-    name: str
-    description: str | None = None
-    price: float
-    tax: float | None = None
-    tags: set[str] = set()
-    
-@app.post("/items/", response_model=Item, tags=["items"])
-async def create_item(item: Item):
-    return item
 
 @app.get("/items/", tags=["items"])
 async def read_items():
     return [{"name": "Foo", "price": 42}]
 
+
 @app.get("/users/", tags=["users"])
 async def read_users():
-    return [{"username": "John Doe"}]
+    return [{"username": "johndoe"}]
+
+
+@app.get("/elements/", tags=["items"], deprecated=True)
+async def read_elements():
+    return [{"item_id": "Foo"}]
