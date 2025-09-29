a9accdc1fe72b55c389e2e9132db4856f2890df6
rashonwhite <rashonwhite684@gmail.com>
2025-09-29 12:00:30 -0500

    refactor: return custom data

--- .venv/main.py
+++ .venv/main.py
@@ -1,22 +1,18 @@
-from datetime import datetime
-from typing import Union
-
-from fastapi import FastAPI
-from fastapi.encoders import jsonable_encoder
-from fastapi.responses import JSONResponse
-from pydantic import BaseModel
-
-
-class Item(BaseModel):
-    title: str
-    timestamp: datetime
-    description: Union[str, None] = None
-
+from fastapi import FastAPI, Response
 
 app = FastAPI()
 
 
-@app.put("/items/{id}")
-def update_item(id: str, item: Item):
-    json_compatible_item_data = jsonable_encoder(item)
-    return JSONResponse(content=json_compatible_item_data)
+@app.get("/legacy/")
+def get_legacy_data():
+    data = """<?xml version="1.0"?>
+    <shampoo>
+    <Header>
+        Apply shampoo here.
+    </Header>
+    <Body>
+        You'll have to use soap here.
+    </Body>
+    </shampoo>
+    """
+    return Response(content=data, media_type="application/xml")
